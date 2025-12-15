08950de93a89cf5c2d60aa506c6bd56702caed28
Savio Jude D'Costa <savio.jude.dcosta@avanade.com>
2025-12-15 13:16:51 +1100

    updated aks-code

--- terraform-manifests/09-aks-cluster-linux-user-nodepools.tf
+++ terraform-manifests/09-aks-cluster-linux-user-nodepools.tf
@@ -1,5 +1,5 @@
 # Create Linux Azure AKS Node Pool
-/*
+
 resource "azurerm_kubernetes_cluster_node_pool" "linux101" {
   #availability_zones    = [1, 2, 3]
   # Added June 2023
@@ -29,4 +29,4 @@ resource "azurerm_kubernetes_cluster_node_pool" "linux101" {
     "app"           = "java-apps"
   }
 }
-*/
+
